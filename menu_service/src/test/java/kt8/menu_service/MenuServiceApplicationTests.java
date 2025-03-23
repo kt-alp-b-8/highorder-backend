9f6f003268040487be3096696900537b0fe08787
KimSeongHo <kksshh0612@gmail.com>
2025-03-24 00:30:10 +0900

    init msa project structure

---
+++ menu_service/src/test/java/kt8/menu_service/MenuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kt8.menu_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
